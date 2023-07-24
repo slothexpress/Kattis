747fefd2f55ac208dff6914cc9d09986907cd7d8
Sami <hellosami@protonmail.com>
2023-07-24 14:08:19 +0200

    Refactor scanner and clean up

--- src/Level2/problemB.java
+++ src/Level2/problemB.java
@@ -9,38 +9,31 @@ public class problemB {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int nNumberOfCases = Integer.parseInt(scanner.nextLine());
-        scanner.close();
-
         while(nNumberOfCases != 0) {
-
-            calculateOptimalCost();
-
+            calculateOptimalCostForCase(scanner);
             nNumberOfCases--;
         }
+        scanner.close();
     }
 
-    private static void calculateOptimalCost() {
-        // Step 1: Setup Coordinates
-        List<double[]> islands = readIslandCoordinates();
-
-        // Step 2: Calculate Distances
+    private static void calculateOptimalCostForCase(Scanner scanner) {
+        List<double[]> islands = readIslandCoordinates(scanner);
         double[][] distances = calculateDistances(islands);
 
-        // Now the 'distances' 2D array contains the distances between all pairs of islands.
-        // You can use this array for further processing to find the optimal cost.
+        // Find the Minimum Spanning Tree (MPT) using Prim's Algorithm
+        double minCost = applyPrimsAlgorithm(distances);
+        System.out.println(minCost);
     }
 
-    private static List<double[]>  readIslandCoordinates() {
+    private static List<double[]> readIslandCoordinates(Scanner scanner) {
         List<double[]> islands = new ArrayList<>();
 
-        Scanner scanner = new Scanner(System.in);
         int mNumberOfIslands = Integer.parseInt(scanner.nextLine());
         for(int i = 1; i <= mNumberOfIslands; i++) {
             String input = scanner.nextLine();
             String coordinates[] = input.split(" ");
             double x = Double.parseDouble(coordinates[0]);
             double y = Double.parseDouble(coordinates[1]);
-
             double[] island = {x, y};
             islands.add(island);
         }
@@ -60,13 +53,58 @@ public class problemB {
 
                 double distance = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 
-                // Since the graph is undirected, we store the distance in both directions.
+                // Store the distance in both directions.
                 distances[i][j] = distance;
                 distances[j][i] = distance;
             }
         }
 
         return distances;
+    }
+
+    private static double applyPrimsAlgorithm(double[][] distances) {
+        int numberOfIslands = distances.length;
+        boolean[] visited = new boolean[numberOfIslands];
+        double[] costs = new double[numberOfIslands];
+
+        // Step 1: Initialize arrays
+        for (int i = 0; i < numberOfIslands; i++) {
+            costs[i] = Double.MAX_VALUE;
+            visited[i] = false;
+        }
+
+        // Step 2: Start with the first island (index 0)
+        costs[0] = 0.0; // The cost to reach itself is set to 0.
+
+        // Step 3: Grow the Minimum Spanning Tree (MST) by iteratively adding the next island
+        for (int step = 0; step < numberOfIslands - 1; step++) {
+
+            // Step 3.1: Find the unvisited island with the minimum cost to add to the MST
+            int nextIsland = -1;
+            for (int island = 0; island < numberOfIslands; island++) {
+                if (!visited[island] && (nextIsland == -1 || costs[island] < costs[nextIsland])) {
+                    nextIsland = island;
+                }
+            }
+
+            // Step 3.2: Mark the next island as visited (it is now included in the MST)
+            visited[nextIsland] = true;
+
+            // Step 3.3: Update the costs of neighboring islands based on the next island
+            for (int island = 0; island < numberOfIslands; island++) {
+                if (!visited[island] && distances[nextIsland][island] < costs[island]) {
+                    costs[island] = distances[nextIsland][island];
+                }
+            }
+        }
+
+        // Step 4: Calculate the total bridge length (minimum spanning tree cost).
+        double totalCost = 0.0;
+        for (double bridgeCost : costs) {
+            totalCost = totalCost + bridgeCost;
+        }
+
+        return totalCost;
     }
 
 }

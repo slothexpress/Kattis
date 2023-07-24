1b42c0a8910d5fe94436f1a43eb0a4359d675bd8
Sami <hellosami@protonmail.com>
2023-07-24 10:47:56 +0200

    Represent coordinates in list

--- src/Level2/problemB.java
+++ src/Level2/problemB.java
@@ -1,23 +1,40 @@
 package Level2;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
 
 public class problemB {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        int numberOfCases = Integer.parseInt(scanner.nextLine());
-        while(numberOfCases != 0) {
-            int numberOfIslands = Integer.parseInt(scanner.nextLine());
-
-            calcalate();
-
-            numberOfCases--;
-        }
+        int nNumberOfCases = Integer.parseInt(scanner.nextLine());
         scanner.close();
+
+        while(nNumberOfCases != 0) {
+
+
+            calculateOptimalCost();
+
+            nNumberOfCases--;
+        }
     }
 
-    private static void calcalate() {
+    private static void calculateOptimalCost() {
+        Scanner scanner = new Scanner(System.in);
+        int mNumberOfIslands = Integer.parseInt(scanner.nextLine());
+        List<double[]> islands = new ArrayList<>();
+
+        for(int i = 1; i <= mNumberOfIslands; i++) {
+            String input = scanner.nextLine();
+            String coordinates[] = input.split(" ");
+            double x = Double.parseDouble(coordinates[0]);
+            double y = Double.parseDouble(coordinates[1]);
+
+            double[] island = {x, y};
+            islands.add(island);
+        }
+
     }
 
 }

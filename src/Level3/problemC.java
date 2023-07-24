a6d952cf3ba2781d778e637c598686fa9be5076f
Sami <hellosami@protonmail.com>
2023-07-24 14:09:33 +0200

    Add skeleton for problemC

---
+++ src/Level3/problemC.java
@@ -0,0 +1,21 @@
+package Level3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class problemC {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int nNumberOfCases = Integer.parseInt(scanner.nextLine());
+        while(nNumberOfCases != 0) {
+
+            
+            nNumberOfCases--;
+        }
+        scanner.close();
+    }
+
+
+}

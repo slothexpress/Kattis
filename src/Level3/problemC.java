c417dbc0001e67c5b54d7f9c88caa7966f4ba9bc
Sami <hellosami@protonmail.com>
2023-07-24 14:28:18 +0200

    Add method to read boardgame input for each case

--- src/Level3/problemC.java
+++ src/Level3/problemC.java
@@ -1,21 +1,29 @@
 package Level3;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-
 
 public class problemC {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int nNumberOfCases = Integer.parseInt(scanner.nextLine());
-        while(nNumberOfCases != 0) {
+        int numberOfCases = Integer.parseInt(scanner.nextLine());
 
-            
-            nNumberOfCases--;
+        while (numberOfCases != 0) {
+            solve(scanner);
+            numberOfCases--;
         }
+
         scanner.close();
     }
+    private static void solve(Scanner scanner) {
+        // Get first line for each case/game: number of tiles
+        int n = Integer.parseInt(scanner.nextLine());
+        int[][] board = new int[n][n];
 
-
+        for (int i = 0; i < n; i++) {
+            String line = scanner.nextLine();
+            for (int j = 0; j < n; j++) {
+                board[i][j] = Character.getNumericValue(line.charAt(j));
+            }
+        }
+        // Now we have the board configuration in the 'board' variable.
+    }
 }

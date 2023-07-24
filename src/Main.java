18de4a8e5497d682dda3db5a4a2fc81cf8520f79
Sami <hellosami@protonmail.com>
2023-07-24 08:46:07 +0200

    Modify code to process data line by line

--- src/Main.java
+++ src/Main.java
@@ -5,35 +5,39 @@ public class Main {
         String inputFileName = "C:/Users/hello/Documents/repos/FileReaderWriter/src/STDINPUT.txt";
         String outputFileName = "C:/Users/hello/Documents/repos/FileReaderWriter/src/STDOUTPUT.txt";
 
-        try {
-            // Read data from input file
-            BufferedReader reader = new BufferedReader(new FileReader(inputFileName));
-            StringBuilder inputData = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                inputData.append(line).append("\n");
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(inputFileName));
+                BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Process the data line by line (You can implement your processing logic here)
+                    String processedLine = processData(line);
+
+                    // Write the processed data immediately to the output file
+                    writer.write(processedLine);
+                    writer.newLine();
+                }
+
+                reader.close();
+                writer.close();
+
+            } catch (IOException e) {
+                System.err.println("Error: " + e.getMessage());
             }
-            reader.close();
-
-            // Process the data (You can implement your processing logic here)
-            String processedData = processData(inputData.toString());
-
-            // Write data to output file (create or replace)
-            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName));
-            writer.write(processedData);
-            writer.close();
-            System.out.println("Data successfully processed and written to " + outputFileName);
-
-        } catch (IOException e) {
-            System.err.println("Error: " + e.getMessage());
         }
-    }
 
     // Implement your data processing logic here
     private static String processData(String data) {
         // You can write your processing logic here
-        // For example, you can manipulate the data, perform calculations, etc.
-        // For now, let's just return the same data as it is.
-        return data;
+        String dataArray[] = data.split(" ");
+
+        long num1 = Long.parseLong(dataArray[0]);
+        long num2 = Long.parseLong(dataArray[1]);
+
+        if (num1 > num2) {
+            return String.valueOf(num1 - num2);
+        }
+        return String.valueOf(num2 - num1);
     }
 }
